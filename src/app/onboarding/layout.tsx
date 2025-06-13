bcc80f1fff5c5d8e31a8d93b93cf50a784fc2f75
Soumya <121118561+SoumyaTejaswi@users.noreply.github.com>
2025-06-13 09:34:19 -0400

    Update layout.tsx

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,16 +1,11 @@
-import { auth } from "@clerk/nextjs/server"
-import { redirect } from "next/navigation"
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 
-export default async function RootLayout(
-  {
-    children,
-  }: {
-    children: React.ReactNode
-  }
-) {
-
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  // Check if a user has completed onboarding
+  // If yes, redirect them to /dashboard
   if ((await auth()).sessionClaims?.metadata?.onboardingComplete === true) {
-    redirect("/dashboard")
+    redirect('/dashboard')
   }
 
   return <>{children}</>
