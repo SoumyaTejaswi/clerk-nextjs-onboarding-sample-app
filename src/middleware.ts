1ebcd82d0ed7c234c315d4366313aedd601541a6
Soumya <121118561+SoumyaTejaswi@users.noreply.github.com>
2025-06-13 09:32:56 -0400

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,32 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-const isPublicRoute = createRouteMatcher(["/", "/onboarding"])
+const isPublicRoute = createRouteMatcher(['/', '/onboarding'])
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId, sessionClaims, redirectToSignIn } = await auth();
+  const { userId, sessionClaims, redirectToSignIn } = await auth()
 
   // If the user isn't signed in and the route is private, redirect to sign-in
   if (!userId && !isPublicRoute(req)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
+    return redirectToSignIn({ returnBackUrl: req.url })
   }
 
   // Catch users who do not have `onboardingComplete: true` in their publicMetadata
   // Redirect them to the /onboading route to complete onboarding
-  if (userId && !sessionClaims?.metadata?.onboardingComplete && req.nextUrl.pathname !== "/onboarding") {
-    const onboardingUrl = new URL("/onboarding", req.url);
-    return NextResponse.redirect(onboardingUrl);
+  if (
+    userId &&
+    !sessionClaims?.metadata?.onboardingComplete &&
+    req.nextUrl.pathname !== '/onboarding'
+  ) {
+    const onboardingUrl = new URL('/onboarding', req.url)
+    return NextResponse.redirect(onboardingUrl)
   }
 
   // If the user is logged in and the route is protected, let them view.
   if (userId && !isPublicRoute(req)) {
-    return NextResponse.next();
+    return NextResponse.next()
   }
-
-});
+})
 
 export const config = {
   matcher: [
@@ -32,5 +35,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
-
+}
